3989a960409f13dca031d92b27ff8514ac44733d
Georgy Buranov <gburanov@gmail.com>
2016-04-17 11:31:45 +0200

    add basic server

--- sinatra_app/server.rb
+++ sinatra_app/server.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
 
 get '/' do
-  "Hello World!"
+  erb :index
 end
+
+post '/email' do
+  @email =  params[:email]
+  puts @email.to_s
+  redirect "/"
+end
+
+__END__
+@@ index
+<html>
+  <body>
+     <h1>Send request to SQS</h1>
+     <form action="/email" method="POST">
+       <input type="text" name="email" value="gburanov@gmail.com"></input>
+       <input type="submit" value="Submit"></input>
+     </form>
+  </body>
+</html>
